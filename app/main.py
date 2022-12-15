b04d66a2cfe82b574c46e97e0d57b32e92684f98
sikso1897 <sikso1897@gmail.com>
2022-12-16 05:22:10 +0900

    apply black

--- app/main.py
+++ app/main.py
@@ -3,12 +3,12 @@ from mangum import Mangum
 
 from api.v1.api import router as api_router
 
-app = FastAPI(title='Serverless Lambda FastAPI')
+app = FastAPI(title="Serverless Lambda FastAPI")
 
 app.include_router(api_router, prefix="/api/v1")
 
 
-@app.get("/",  tags=["Endpoint Test"])
+@app.get("/", tags=["Endpoint Test"])
 def main_endpoint_test():
     return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
 
--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -3,9 +3,11 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_main_resource():
     response_auth = client.get("/")
     assert response_auth.status_code == 200
+
 
 def test_child_resource():
     response_auth = client.get("/api/v1/test")
